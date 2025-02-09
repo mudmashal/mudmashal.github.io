e898788ecc620e1c7f16c9229e95f7be46b8b1c8
mudmashal <mudasser500tk@icloud.com>
2025-02-09 14:19:27 -0800

    Added initial structure to website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+     anchor.addEventListener('click', function(e) {
+         e.preventDefault();
+         const targetSection = document.querySelector(this.getAttribute('href'));
+         targetSection.scrollIntoView({ behavior: 'smooth' });
+     });
+ });
+ 
